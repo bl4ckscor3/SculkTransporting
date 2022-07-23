ce0f6fc46ea412d48ff5bcade840060ffefdb39f
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-07-23 15:11:57 +0200

    fix default name of sculk barrel block entity

--- src/main/java/sculktransporting/blockentities/SculkBarrelBlockEntity.java
+++ src/main/java/sculktransporting/blockentities/SculkBarrelBlockEntity.java
@@ -1,10 +1,12 @@
 package sculktransporting.blockentities;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.entity.BarrelBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import sculktransporting.registration.STBlockEntityTypes;
+import sculktransporting.registration.STBlocks;
 
 public class SculkBarrelBlockEntity extends BarrelBlockEntity {
 	public SculkBarrelBlockEntity(BlockPos pos, BlockState state) {
@@ -14,5 +16,10 @@ public class SculkBarrelBlockEntity extends BarrelBlockEntity {
 	@Override
 	public BlockEntityType<?> getType() {
 		return STBlockEntityTypes.SCULK_BARREL_BLOCK_ENTITY.get();
+	}
+
+	@Override
+	protected Component getDefaultName() {
+		return Component.translatable(STBlocks.SCULK_BARREL.get().getDescriptionId());
 	}
 }
