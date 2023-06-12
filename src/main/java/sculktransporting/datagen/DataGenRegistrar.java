cac2ec4a4b56e179c53c49e920c4e12518631e3c
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-06-12 14:51:21 +0200

    add datagen for pack.mcmeta

--- src/main/java/sculktransporting/datagen/DataGenRegistrar.java
+++ src/main/java/sculktransporting/datagen/DataGenRegistrar.java
@@ -1,15 +1,23 @@
 package sculktransporting.datagen;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import net.minecraft.DetectedVersion;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.loot.LootTableProvider.SubProviderEntry;
+import net.minecraft.data.metadata.PackMetadataGenerator;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
@@ -33,5 +41,11 @@ public class DataGenRegistrar {
 		generator.addProvider(event.includeServer(), blockTagGenerator);
 		generator.addProvider(event.includeServer(), new ItemTagGenerator(output, lookupProvider, blockTagGenerator.contentsGetter(), existingFileHelper));
 		generator.addProvider(event.includeServer(), new RecipeGenerator(output));
+		//@formatter:off
+		generator.addProvider(true, new PackMetadataGenerator(output)
+                .add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("Sculk Transporting resources & data"),
+                        DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES),
+                        Arrays.stream(PackType.values()).collect(Collectors.toMap(Function.identity(), DetectedVersion.BUILT_IN::getPackVersion)))));
+		//@formatter:on
 	}
 }
